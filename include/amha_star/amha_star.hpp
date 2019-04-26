0d874404ac95d69010b594ead0523d3c74c0ca5f
nrkumar93 <nrkumar93@gmail.com>
2019-04-25 22:28:59 -0400

    cleaning up

--- include/amha_star/amha_star.hpp
+++ include/amha_star/amha_star.hpp
@@ -48,30 +48,32 @@ namespace csbpl_planner
 {
   
   template<typename Environment,
-           typename State,
-           typename HeapValueType>
-  class AMHAStar : public Dijkstra<Environment, State, HeapValueType>
+    typename State,
+    typename HeapValueType>
+  class AMHAStar : public Dijkstra<Environment,
+    State,
+    HeapValueType>
   {
   
   public:
     
     typedef typename Environment::Ptr EnvironmentPtr;
     typedef Vertex<State,
-                   HeapValueType> SearchVertex;
+    HeapValueType> SearchVertex;
     typedef typename SearchVertex::Ptr SearchVertexPtr;
     typedef typename State::Ptr StatePtr;
     typedef typename State::Compare StateCompare;
     
     AMHAStar(EnvironmentPtr& env_ptr,
-            double time_limit,
-            double epsilon,
-            double mha_epsilon,
-            double del_epsilon,
-            double del_mha_epsilon,
-            int num_inad) : Dijkstra<Environment,
-                                     State,
-                                     HeapValueType>(env_ptr,
-                                                    time_limit)
+             double time_limit,
+             double epsilon,
+             double mha_epsilon,
+             double del_epsilon,
+             double del_mha_epsilon,
+             int num_inad) : Dijkstra<Environment,
+      State,
+      HeapValueType>(env_ptr,
+                     time_limit)
     {
       this->params_.eps_ = epsilon;
       this->params_.mha_eps_ = mha_epsilon;
@@ -83,29 +85,31 @@ namespace csbpl_planner
       
       open_inad_.reserve(num_inad);
     }
-  
+    
     virtual bool expand(SearchVertexPtr& vertex)
     {
       if (vertex->heap_index_ != -1)
       {
-        this->open_.decreaseKey(vertex, -INFINITY);
+        this->open_.decreaseKey(vertex,
+                                -INFINITY);
         this->open_.remove();
         vertex->key_ = INFINITY;
       }
-    
+      
       for (int i=0; i<this->params_.num_inad_; ++i)
       {
         if (vertex->mha_copy_[i]->heap_index_ != -1)
         {
-          open_inad_[i].decreaseKey(vertex->mha_copy_[i], -INFINITY);
+          open_inad_[i].decreaseKey(vertex->mha_copy_[i],
+                                    -INFINITY);
           open_inad_[i].remove();
           vertex->mha_copy_[i]->key_ = INFINITY;
         }
       }
-    
+      
       std::vector<SearchVertexPtr> vertex_successors =
         this->env_ptr_->getValidSuccessors(vertex);
-    
+      
       for(const SearchVertexPtr &successor : vertex_successors)
       {
         if (successor->mha_copy_.empty())
@@ -117,17 +121,18 @@ namespace csbpl_planner
             successor->mha_copy_[i]->self_ptr_ = successor;
           }
         }
-      
+        
         double new_cost = vertex->g_cost_ +
-                          this->env_ptr_->getCostToSuccessor(vertex->state_, successor->state_);
-      
+                          this->env_ptr_->getCostToSuccessor(vertex->state_,
+                                                             successor->state_);
+        
         if (new_cost < successor->g_cost_)
         {
           successor->g_cost_ = new_cost;
           successor->parent_vertex_ = vertex;
           successor->root_id_ = vertex->root_id_;
           successor->depth_ = successor->parent_vertex_->depth_ + 1;
-  
+          
           if (this->closed_.find(successor->state_) != this->closed_.end())
           {
             incons_.push_back(successor->state_);
@@ -151,7 +156,7 @@ namespace csbpl_planner
               this->open_.decreaseKey(successor,
                                       f_cost_ad);
             }
-  
+            
             if (closed_inad_.find(successor->state_) != closed_inad_.end())
             {
               incons_inad_.push_back(successor->state_);
@@ -163,7 +168,7 @@ namespace csbpl_planner
                 double f_cost_inad = successor->g_cost_ +
                                      this->params_.eps_*
                                      this->env_ptr_->getInadmissibleHeuristicCost(successor->state_, i);
-    
+                
                 if (f_cost_inad <= this->params_.mha_eps_*f_cost_ad)
                 {
                   if (successor->mha_copy_[i]->heap_index_ == -1)
@@ -191,22 +196,24 @@ namespace csbpl_planner
     virtual bool improvePath()
     {
 #if DEBUG
-  int num_expansions = 0;
-  double nearest_node_to_goal_dist = std::numeric_limits<double>::infinity();
+      int num_expansions = 0;
+  double nearest_node_to_goal_dist = INFINITY;
   SearchVertexPtr nearest_node_to_goal;
 #endif
       this->anytime_stats_.num_expansions_.push_back(0);
-      std::cout << "Index: " << this->anytime_stats_.num_expansions_.size() << " ";
+      std::cout << "[AMHAStar::improvePath] Improve path iteration: "
+                << this->anytime_stats_.num_expansions_.size() << std::endl;
       while (this->open_.top()->key_ < INFINITY)
       {
-  
+        
         double time_elapsed =
           duration_cast<duration<double> >(Clock::now() -
                                            this->params_.start_time_).count();
-  
+        
         if (time_elapsed > this->params_.time_limit_)
         {
-          std::cout << "[AMHAStar::improvePath] Time limit exceeded... Exiting!" << std::endl;
+          std::cout << "[AMHAStar::improvePath] Time limit exceeded..."
+                       " Exiting!" << std::endl;
           return false;
         }
         
@@ -217,7 +224,8 @@ namespace csbpl_planner
           {
             if (this->goal_vertex_->g_cost_ < open_inad_[i].top()->key_)
             {
-              std::cout << "Goal node expanded. Premature termiation!" << std::endl;
+              std::cout << "[AMHAStar::improvePath] Goal node expanded. "
+                           "Premature termiation!" << std::endl;
               return true;
             }
             else
@@ -236,7 +244,8 @@ namespace csbpl_planner
           {
             if (this->goal_vertex_->g_cost_ < this->open_.top()->key_)
             {
-              std::cout << "Goal node expanded. Premature termiation!" << std::endl;
+              std::cout << "[AMHAStar::improvePath] Goal node expanded. "
+                           "Premature termiation!" << std::endl;
               return true;
             }
             else
@@ -255,21 +264,23 @@ namespace csbpl_planner
       }
 
 #if DEBUG
-      std::cout << "Total number of expansions: " << num_expansions << std::endl;
+      std::cout << "[AMHAStar::improvePath] Total number of expansions: "
+                << num_expansions << std::endl;
 #endif
-  
-  
+      
+      
       if (this->open_.size() == 0)
       {
 #if DEBUG
-        std::cout << "Open list is empty! Error... Returning nearest node reached" << std::endl;
-    this->env_ptr_->goal_ = nearest_node_to_goal->state_;
-    return true;
+        std::cout << "[AMHAStar::improvePath] Open list is empty! Error... "
+                     "Returning nearest node reached" << std::endl;
+        this->env_ptr_->goal_ = nearest_node_to_goal->state_;
+        return true;
 #endif
-        std::cout << "Open list is empty! Error... " << std::endl;
+        std::cout << "[AMHAStar::improvePath] Open list is empty! Error... " << std::endl;
         return false;
       }
-  
+      
       return true;
     }
     
@@ -311,33 +322,36 @@ namespace csbpl_planner
         this->goal_vertex_->mha_copy_.push_back(vertex_copy);
         this->goal_vertex_->mha_copy_[i]->self_ptr_ = this->goal_vertex_;
       }
-  
+      
       // Start timer
       this->params_.start_time_ = Clock::now();
-  
+      
       // Suboptimality bound on current solution
+      // @TODO Report the tigher suboptimality as done in the ARA Star paper.
 //      for (const auto& incons_node: incons_)
 //      {
 //        double f_incons = this->env_ptr_->explored_[incons_node]->g_cost_ +
 //          this->params_.eps_*this->env_ptr_->getAdmissibleHeuristicCost(incons_node);
 //      }
-  
+      
       if (improvePath())
       {
         std::vector<StatePtr> path =
           this->backtrackPath(this->env_ptr_->getGoalState());
         anytime_stats_.solution_paths_.push_back(path);
         anytime_stats_.solution_eps_.push_back(this->params_.eps_*this->params_.mha_eps_);
-        double unit_solution_time = duration_cast<duration<double> >(Clock::now() - this->params_.start_time_).count();
+        double unit_solution_time =
+          duration_cast<duration<double> >(Clock::now() -
+                                           this->params_.start_time_).count();
         anytime_stats_.solution_time_.push_back(unit_solution_time);
         anytime_stats_.cummulative_time_.push_back(duration_cast<duration<double> >(Clock::now() -
                                                                                     this->params_.start_time_).count());
       }
-  
+      
       double time_elapsed =
         duration_cast<duration<double> >(Clock::now() -
                                          this->params_.start_time_).count();
-  
+      
       // Updating w1 and w2
       this->params_.eps_ -= this->params_.delta_eps_;
       this->params_.mha_eps_ -= this->params_.delta_mha_eps_;
@@ -364,17 +378,17 @@ namespace csbpl_planner
                                  this->params_.eps_*this->env_ptr_->getAdmissibleHeuristicCost(open_vertex->state_);
           open_u_incons.push_back(open_vertex);
         }
-    
+        
         for (const SearchVertexPtr& new_open : open_u_incons)
         {
           this->open_.insert(new_open);
           this->open_.decreaseKey(new_open,
                                   new_open->f_cost_);
         }
-    
+        
         this->closed_.clear();
-  
-  
+        
+        
         // OPEN_INAD U INCONS_INAD and clearing CLOSED_INAD
         for (int i=0; i<this->params_.num_inad_; ++i)
         {
@@ -384,7 +398,8 @@ namespace csbpl_planner
           {
             SearchVertexPtr incons_inad_vertex = this->env_ptr_->explored_[incons_inad_state];
             incons_inad_vertex->mha_copy_[i]->f_cost_ = incons_inad_vertex->g_cost_ +
-                                          this->params_.eps_*this->env_ptr_->getInadmissibleHeuristicCost(incons_inad_state, i);
+                                                        this->params_.eps_ *
+                                                        this->env_ptr_->getInadmissibleHeuristicCost(incons_inad_state, i);
             open_inad_u_incons_inad.push_back(incons_inad_vertex->mha_copy_[i]);
           }
           while (open_inad_[i].size() > 0)
@@ -392,20 +407,21 @@ namespace csbpl_planner
             SearchVertexPtr open_inad_vertex =
               std::static_pointer_cast<SearchVertex>(open_inad_[i].remove());
             open_inad_vertex->f_cost_ = open_inad_vertex->self_ptr_->g_cost_ +
-                                        this->params_.eps_*this->env_ptr_->getInadmissibleHeuristicCost(open_inad_vertex->state_, i);
+                                        this->params_.eps_ *
+                                        this->env_ptr_->getInadmissibleHeuristicCost(open_inad_vertex->state_, i);
             open_inad_u_incons_inad.push_back(open_inad_vertex);
           }
-  
+          
           for (const SearchVertexPtr& new_open : open_inad_u_incons_inad)
           {
             open_inad_[i].insert(new_open);
             open_inad_[i].decreaseKey(new_open,
-                                    new_open->f_cost_);
+                                      new_open->f_cost_);
           }
         }
-  
+        
         closed_inad_.clear();
-  
+        
         if (improvePath())
         {
           std::vector<StatePtr> path =
@@ -413,38 +429,40 @@ namespace csbpl_planner
           anytime_stats_.solution_paths_.push_back(path);
           anytime_stats_.solution_eps_.push_back(this->params_.eps_*this->params_.mha_eps_);
           double unit_solution_time = anytime_stats_.solution_time_.size()==0?
-                                      duration_cast<duration<double> >(Clock::now() - this->params_.start_time_).count():
-                                      duration_cast<duration<double> >(Clock::now() - this->params_.start_time_).count() -
+                                      duration_cast<duration<double> >(Clock::now() -
+                                                                       this->params_.start_time_).count():
+                                      duration_cast<duration<double> >(Clock::now() -
+                                                                       this->params_.start_time_).count() -
                                       time_elapsed;
           anytime_stats_.solution_time_.push_back(unit_solution_time);
           anytime_stats_.cummulative_time_.push_back(duration_cast<duration<double> >(Clock::now() -
                                                                                       this->params_.start_time_).count());
         }
-    
+        
         // Epsilon update
         this->params_.eps_ -= this->params_.delta_eps_;
         this->params_.mha_eps_ -= this->params_.delta_mha_eps_;
-  
+        
         // Time elapsed update
         time_elapsed =
           duration_cast<duration<double> >(Clock::now() -
                                            this->params_.start_time_).count();
-    
+        
       }
-  
+      
       if (time_elapsed > this->params_.time_limit_ &&
           anytime_stats_.solution_paths_.size()==0)
       {
         std::cout << "[AMHAStar::run] Time limit exceeded and"
-          " no solution found... Exiting!" << std::endl;
+                     " no solution found... Exiting!" << std::endl;
         return false;
       }
-  
+      
       return true;
-
+      
     }
-  
-  
+    
+    
     virtual AnytimePlannerStats<State> getAnytimePlannerStats()
     {
       return anytime_stats_;
@@ -455,15 +473,15 @@ namespace csbpl_planner
     std::vector<csbpl_common::Heap<HeapValueType> > open_inad_;
     
     std::unordered_map<StatePtr,
-                       bool,
-                       std::hash<StatePtr>,
-                       StateCompare> closed_inad_;
-  
+      bool,
+      std::hash<StatePtr>,
+      StateCompare> closed_inad_;
+    
     std::vector<StatePtr> incons_;
     std::vector<StatePtr> incons_inad_;
-  
+    
     AnytimePlannerStats<State> anytime_stats_;
-  
+    
   };
   
 } // namespace csbpl_planner
